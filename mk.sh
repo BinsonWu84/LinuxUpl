31258bd4c6fb6a2ae13bb6efe861d9b773d3334f
Wu, Binson <binson.wu@intel.com>
2022-09-23 16:56:40 +0800

    Do not clone whole site-local and only download linuxboot_bzImage.

--- mk.sh
+++ mk.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-echo Checkout out LinuxUpl ...
+echo Parsing LinuxUpl ...
+TOKEN=$(cat ~/.netrc | grep -m 1 password | cut -d " " -f 4)
+
+echo Checkout LinuxUpl ...
 if [ ! -d "LinuxUpl" ]; then
   git clone https://github.com/intel-innersource/firmware.boot.upl.linux-upl LinuxUpl
+else
+  echo "  LinuxUpl exist!"
 fi
 
-echo Checkout out site-local ...
-if [ ! -d "site-local" ]; then
-  git clone https://github.com/intel-innersource/firmware.boot.coreboot-xeon.site-local site-local
+echo Checkout Linux Boot Image ...
+if [ ! -f "linuxboot_bzImage" ]; then
+  wget --header 'Authorization: token '$TOKEN https://raw.githubusercontent.com/intel-innersource/firmware.boot.coreboot-xeon.site-local/main/payload/LinuxBoot/linuxboot_bzImage
+else
+  echo "  Linux Boot Image exist!"
 fi
 
 echo Compiling LinuxUpl ...
 cd LinuxUpl
 rm -rf Build
-bash LinuxUplBuild.sh -K ../site-local/payload/LinuxBoot/linuxboot_bzImage
+bash LinuxUplBuild.sh -K ../linuxboot_bzImage
 cd ..
 
 cp -f ./LinuxUpl/Build/LinuxUniversalPayload.elf ./
